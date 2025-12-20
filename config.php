40ac90b39ce088ec0bf3c5691f509213b1e6d667
Hilmy Fhauzan <hilmyfhauzan8@gmail.com>
2025-12-20 17:10:39 +0800

    Fix config.php

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 $db_host = 'localhost'; // Nama server
 $db_user = 'root'; // User server
 $db_pass = ''; // Password server
-$db_name = 'latihan-phpmyyy'; // Nama database
+$db_name = 'globalin'; // Nama database
 
 $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 if (!$conn) {

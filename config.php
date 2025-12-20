a3d921879b07a58b5c05e752d8fe1f9ed179530f
Hilmy Fhauzan <hilmyfhauzan8@gmail.com>
2025-12-20 17:07:18 +0800

    Add config.php

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+$db_host = 'localhost'; // Nama server
+$db_user = 'root'; // User server
+$db_pass = ''; // Password server
+$db_name = 'latihan-phpmyyy'; // Nama database
+
+$conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+if (!$conn) {
+    die('Gagal terhubung MySQL: ' . mysqli_connect_error());
+};
+
+?>
